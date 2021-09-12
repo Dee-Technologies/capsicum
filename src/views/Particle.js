20414ec8c5bb2014dcf3704d1981d349e983163c
dillondesilva <dillon.desilva@geeksdiary.com>
2021-09-12 13:55:26 +1000

    Initial particle data is now randomly generated

--- src/views/Particle.js
+++ src/views/Particle.js
@@ -9,21 +9,40 @@ class Particle extends React.Component {
             speed: 0.5,
             top: 50,
             left: 50,
-            size: 1
+            size: 1,
+            initialX: 0,
+            initialY: 0
         }
 
-        this.maxSize = 15;
+        this.maxSize = 8;
+        this.minSize = 1;
+
+        this.maxSpeed = 1;
     }
 
     componentDidMount() {
-        setInterval(() => {
-            this.setState({
-                top: this.state.top + this.state.speed,
-                left: this.state.left + this.state.speed
-            }, () => {   
-                console.log(this.state.top, this.state.left)
-            })
-        }, 10);
+        const size = Math.floor(Math.random() * (this.maxSize - this.minSize + 1)) + this.minSize;
+        const initialX = Math.floor(Math.random() * window.innerWidth);
+        const initialY = Math.floor(Math.random() * window.innerHeight);
+        const speed = Math.random() * this.maxSpeed;
+
+        this.setState({
+            top: initialY,
+            left: initialX,
+            initialY: initialY,
+            initialX: initialX,
+            speed: speed,
+            size: size.toString() + "vh"
+        }, () => {
+            this.motionInterval = setInterval(() => {
+                this.setState({
+                    top: this.state.top + this.state.speed,
+                    left: this.state.left + this.state.speed,
+                }, () => {   
+                    console.log(this.state.top, this.state.left)
+                })
+            }, 10);
+        })
     }
 
     // moveParticle() {
@@ -39,7 +58,7 @@ class Particle extends React.Component {
     render() {
         return (
             <div style={{position: "absolute", top: this.state.top, left: this.state.left}} onClick={this.particleClicked}>
-                <img alt="" style={{width: "10vh"}} ref="particle" src={iconLogo}/>
+                <img alt="" style={{width: this.state.size}} ref="particle" src={iconLogo}/>
             </div>
         )
     }
